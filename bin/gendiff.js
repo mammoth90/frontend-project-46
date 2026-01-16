6cb726be3fd6463d9bafe7f3c04c77afda705d3e
mammoth90 <max.mamontov90@gmail.com>
2026-01-16 17:00:19 +0500

    1st stage

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { Command } from "commander"
+const programm = new Command();
+
+programm 
+  .name('gendiff')
+  .description(`Compares two configuration files and shows a difference`)
+  .version('1.0.0.0')
+
+
+programm.parse()
+  

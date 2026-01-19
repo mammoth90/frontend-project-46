2ed193fa373b346c03de28dcc4267e3c272823bd
mammoth90 <max.mamontov90@gmail.com>
2026-01-19 12:57:25 +0500

    2nd stage

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "commander"
+import { Command, Option } from "commander"
 const programm = new Command();
 
 programm 
   .name('gendiff')
   .description(`Compares two configuration files and shows a difference`)
-  .version('1.0.0.0')
+  .argument('<filepath1>', 'input file path')
+  .argument('<filepath2>', 'output file path')
+  .version('1.0')
 
+programm
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv)
 
-programm.parse()
-  
+ 

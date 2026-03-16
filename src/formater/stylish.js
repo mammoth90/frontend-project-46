1e3a51ac4667088feb029f6a4296d943a4d7a4ff
mammoth90 <mamontov.max90@gmail.com>
2026-03-16 13:03:30 +0500

    fix problems with " " in stylish

--- src/formater/stylish.js
+++ src/formater/stylish.js
@@ -1,30 +1,24 @@
 import _ from 'lodash'
 
-const trueValue = (value) => {
-  if (value !== '') {
-    return ` ${value}`
-  }
-  return value
-}
 const printLeaf = (currentNode, depth, replacer) => {
   let node = ''
   const { type, newValue, key, value } = currentNode
   switch (type) {
     case 'removed':
-      node += `${replacer.repeat(depth)}- ${key}:${trueValue(value)}\n`
+      node += `${replacer.repeat(depth)}- ${key}: ${value}\n`
       return node
 
     case 'added':
-      node += `${replacer.repeat(depth)}+ ${key}:${trueValue(value)}\n`
+      node += `${replacer.repeat(depth)}+ ${key}: ${value}\n`
       return node
 
     case 'unchanged':
-      node += `${replacer.repeat(depth)}  ${key}:${trueValue(value)}\n`
+      node += `${replacer.repeat(depth)}  ${key}: ${value}\n`
       return node
 
     case 'changed':
-      node += `${replacer.repeat(depth)}- ${key}:${trueValue(value)}\n`
-      node += `${replacer.repeat(depth)}+ ${key}:${trueValue(newValue)}\n`
+      node += `${replacer.repeat(depth)}- ${key}: ${value}\n`
+      node += `${replacer.repeat(depth)}+ ${key}: ${newValue}\n`
       return node
 
     default:

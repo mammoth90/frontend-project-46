a33453a3f117521979253033c26a891426f5eee9
mammoth90 <mamontov.max90@gmail.com>
2026-03-16 13:07:39 +0500

    delete first line in result

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -18,7 +18,7 @@ programm.action((filepath1, filepath2, options) => {
   const file1 = path.resolve(process.cwd(), filepath1)
   const file2 = path.resolve(process.cwd(), filepath2)
   const result = genDiff(file1, file2, format)
-  console.log(`gendiff --format ${format} ${filepath1} ${filepath2}\n`)
+  // console.log(`gendiff --format ${format} ${filepath1} ${filepath2}\n`)
   console.log(result)
 })
 
